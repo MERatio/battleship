84f57d32effe59ed224db4fa2ace8e3cb6ded422
Mark Edward Ratio <markedwardratio@gmail.com>
2020-11-05 15:08:09 +0800

    Add game module

---
+++ src/modules/game.js
@@ -0,0 +1,25 @@
+import ship from '../factories/ship/ship';
+
+const game = (() => {
+	const _shipCreator = (length, quantity) => {
+		const ships = [];
+		for (let i = 0; i < quantity; i++) {
+			const shipObj = ship(length);
+			ships.push(shipObj);
+		}
+		return ships;
+	};
+
+	const getShips = () => {
+		const ships = [];
+		for (let i = 1; i < 5; i++) {
+			const createdShips = _shipCreator(5 - i, i);
+			ships.push(...createdShips);
+		}
+		return ships;
+	};
+
+	return { getShips };
+})();
+
+export default game;
