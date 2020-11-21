28924834045abc43b42dbe0d8c015958a8f6ee83
Mark Edward Ratio <markedwardratio@gmail.com>
2020-11-21 11:40:07 +0800

    Extract part of code into new component

--- src/components/cell/cell.js
+++ src/components/cell/cell.js
@@ -1,4 +1,5 @@
 import './cell.css';
+import ship from '../ship/ship';
 import helpers from '../../modules/helpers/helpers';
 
 const cell = (
@@ -28,35 +29,13 @@ const cell = (
 		if (helpers.isMissedCell(cell)) {
 			cellDiv.classList.add('cell-missed');
 		} else if (helpers.isPart(cell) && cell.index === 0) {
-			const shipObj = gameboardObj.findShip(cell.shipId);
-			const shipDiv = document.createElement('div');
-			shipDiv.classList.add('ship');
-			if (isMainPlayer) {
-				shipDiv.dataset.ownShip = 'true';
-				shipDiv.dataset.shipId = shipObj.id;
-				shipDiv.setAttribute('draggable', 'true');
-				shipDiv.addEventListener('dragstart', handleShipDragStart);
-				shipDiv.addEventListener('click', handleShipClick);
-			} else {
-				shipDiv.classList.add('pointer-events-none');
-			}
-			if (shipObj.isSunk()) {
-				shipDiv.classList.add('ship-sunk');
-			}
-			if (shipObj.isHorizontal) {
-				shipDiv.classList.add('ship-horizontal');
-			}
-			shipObj.parts.forEach((part) => {
-				const partDiv = document.createElement('div');
-				partDiv.classList.add('part', 'pointer-events-none');
-				if (helpers.isFunctionalPart && isMainPlayer) {
-					partDiv.classList.add('part-functional');
-				} else if (helpers.isPartHit(part)) {
-					partDiv.classList.remove('part-functional');
-					partDiv.classList.add('part-hit');
-				}
-				shipDiv.appendChild(partDiv);
-			});
+			const shipDiv = ship(
+				cell,
+				gameboardObj,
+				isMainPlayer,
+				handleShipDragStart,
+				handleShipClick
+			);
 			cellDiv.appendChild(shipDiv);
 		}
 	}
