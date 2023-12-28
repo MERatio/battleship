a4d9d00a127d9269e0658bdd23bb569807512223
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-12-28 23:15:04 +0800

    Render ships

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@
 import "./style.css";
 import createGameboard from "./js/createGameboard";
 import * as dom from "./js/dom";
+import { generateShips } from "./js/helpers";
 
 function init() {
   const p1Gameboard = createGameboard();
   const p2Gameboard = createGameboard();
-  dom.renderBoard(dom.p1BoardContainer, p1Gameboard.getBoard());
-  dom.renderBoard(dom.p2BoardContainer, p2Gameboard.getBoard());
+  const p1Ships = generateShips();
+  const p2Ships = generateShips();
+  p1Gameboard.randomlyPlaceShips(p1Ships);
+  p2Gameboard.randomlyPlaceShips(p2Ships);
+  dom.renderBoard(dom.p1BoardContainer, p1Gameboard.getBoard(), true);
+  dom.renderBoard(dom.p2BoardContainer, p2Gameboard.getBoard(), false);
 }
 
 init();
--- src/js/dom.js
+++ src/js/dom.js
@@ -1,13 +1,20 @@
 export const p1BoardContainer = document.querySelector(".p1BoardContainer");
 export const p2BoardContainer = document.querySelector(".p2BoardContainer");
 
-export function renderBoard(el, board) {
+export function renderBoard(el, board, showShips) {
   const boardDiv = document.createElement("div");
   boardDiv.classList.add("board");
   board.forEach((row, i) => {
     row.forEach((square, j) => {
       const squareDiv = document.createElement("div");
       squareDiv.classList.add("square");
+      if (
+        showShips &&
+        square &&
+        Object.prototype.hasOwnProperty.call(square, "isSunk")
+      ) {
+        squareDiv.classList.add("ship");
+      }
       squareDiv.dataset.rowIndex = i;
       squareDiv.dataset.columnIndex = j;
       boardDiv.append(squareDiv);
