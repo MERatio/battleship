5617a5dfec3d20d0476760966e9a860252be6575
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-12-28 23:33:00 +0800

    Update boards so user can only interact with p2 board

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,23 +1,32 @@
 export const p1BoardContainer = document.querySelector(".p1BoardContainer");
 export const p2BoardContainer = document.querySelector(".p2BoardContainer");
 
-export function renderBoard(el, board, showShips) {
+function createSquareBtn(square, isActivePlayerBoard, rowIndex, columnIndex) {
+  const squareBtn = document.createElement("button");
+  squareBtn.classList.add("square");
+  if (
+    isActivePlayerBoard &&
+    square &&
+    Object.prototype.hasOwnProperty.call(square, "isSunk")
+  ) {
+    squareBtn.classList.add("ship");
+  }
+  if (isActivePlayerBoard) {
+    squareBtn.classList.add("cursor-auto");
+    squareBtn.setAttribute("disabled", "");
+  }
+  squareBtn.dataset.rowIndex = rowIndex;
+  squareBtn.dataset.columnIndex = columnIndex;
+  return squareBtn;
+}
+
+export function renderBoard(el, board, isActivePlayerBoard) {
   const boardDiv = document.createElement("div");
   boardDiv.classList.add("board");
   board.forEach((row, i) => {
     row.forEach((square, j) => {
-      const squareDiv = document.createElement("button");
-      squareDiv.classList.add("square");
-      if (
-        showShips &&
-        square &&
-        Object.prototype.hasOwnProperty.call(square, "isSunk")
-      ) {
-        squareDiv.classList.add("ship");
-      }
-      squareDiv.dataset.rowIndex = i;
-      squareDiv.dataset.columnIndex = j;
-      boardDiv.append(squareDiv);
+      const squareBtn = createSquareBtn(square, isActivePlayerBoard, i, j);
+      boardDiv.append(squareBtn);
     });
   });
   el.append(boardDiv);
