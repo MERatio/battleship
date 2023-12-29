609dbe7deb607348168b3caf572bb74afd38233b
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-12-29 11:02:04 +0800

    Create players

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 "use strict";
 
 import "./style.css";
-import createGameboard from "./js/createGameboard";
+import createPlayer from "./js/createPlayer";
 import * as dom from "./js/dom";
 import { generateShips } from "./js/helpers";
 
 function init() {
-  const p1Gameboard = createGameboard();
-  const p2Gameboard = createGameboard();
+  const p1 = createPlayer("human");
+  const p2 = createPlayer("computer");
+  const p1Gameboard = p1.gameboard;
+  const p2Gameboard = p2.gameboard;
   const p1Ships = generateShips();
   const p2Ships = generateShips();
   p1Gameboard.randomlyPlaceShips(p1Ships);
