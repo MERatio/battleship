18e93982824fe3ebea7cc07f442a15658452cb0e
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-12-29 22:25:38 +0800

    Add game loop

--- src/index.js
+++ src/index.js
@@ -5,17 +5,83 @@ import createPlayer from "./js/createPlayer";
 import * as dom from "./js/dom";
 import { generateShips } from "./js/helpers";
 
-function init() {
-  const p1 = createPlayer("human");
-  const p2 = createPlayer("computer");
-  const p1Gameboard = p1.gameboard;
-  const p2Gameboard = p2.gameboard;
-  const p1Ships = generateShips();
-  const p2Ships = generateShips();
-  p1Gameboard.randomlyPlaceShips(p1Ships);
-  p2Gameboard.randomlyPlaceShips(p2Ships);
-  dom.renderBoard(dom.p1BoardContainer, p1Gameboard.getBoard(), true);
-  dom.renderBoard(dom.p2BoardContainer, p2Gameboard.getBoard(), false);
+function getWinner() {
+  if (p1.gameboard.areAllShipsSunk()) {
+    return p2;
+  } else if (p2.gameboard.areAllShipsSunk()) {
+    return p1;
+  } else {
+    return null;
+  }
 }
 
-init();
+function renderNewBoards() {
+  dom.clearBoards(
+    [dom.p1BoardContainer, dom.p2BoardContainer],
+    handleSquareBtnClick,
+  );
+  dom.renderBoard(
+    dom.p1BoardContainer,
+    p1.gameboard.getBoard(),
+    activePlayer === p1,
+    handleSquareBtnClick,
+  );
+  dom.renderBoard(
+    dom.p2BoardContainer,
+    p2.gameboard.getBoard(),
+    activePlayer === p2,
+    handleSquareBtnClick,
+  );
+}
+
+function playerAttack(player, rowIndex, columnIndex) {
+  try {
+    if (player === p1) {
+      p1.attack(p2.gameboard, rowIndex, columnIndex);
+    } else {
+      p2.randomAttack(p1.gameboard);
+    }
+    activePlayer = activePlayer === p1 ? p2 : p1;
+    renderNewBoards();
+    const winner = getWinner();
+    if (winner) {
+      // To render the change in board first, then alert.
+      setTimeout(() => {
+        alert(`${winner === p1 ? "Player 1" : "Player 2"} wins!`);
+      }, 0);
+      gameOver = true;
+      dom.disableBoard(
+        activePlayer === p1 ? dom.p2BoardContainer : dom.p1BoardContainer,
+      );
+      return;
+    }
+    if (activePlayer === p2) {
+      playerAttack(p2);
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+
+function handleSquareBtnClick(e) {
+  if (gameOver) {
+    return;
+  }
+
+  const { rowIndex, columnIndex } = e.target.dataset;
+  try {
+    playerAttack(p1, Number(rowIndex), Number(columnIndex));
+  } catch (err) {
+    alert(err);
+  }
+}
+
+const p1 = createPlayer("human");
+const p2 = createPlayer("computer");
+let activePlayer = p1;
+const p1Ships = generateShips();
+const p2Ships = generateShips();
+let gameOver = false;
+p1.gameboard.randomlyPlaceShips(p1Ships);
+p2.gameboard.randomlyPlaceShips(p2Ships);
+renderNewBoards();
--- src/js/dom.js
+++ src/js/dom.js
@@ -1,33 +1,66 @@
 export const p1BoardContainer = document.querySelector(".p1BoardContainer");
 export const p2BoardContainer = document.querySelector(".p2BoardContainer");
 
-function createSquareBtn(square, isActivePlayerBoard, rowIndex, columnIndex) {
+function createSquareBtn(
+  square,
+  isActivePlayerBoard,
+  rowIndex,
+  columnIndex,
+  onSquareBtnClick,
+) {
   const squareBtn = document.createElement("button");
   squareBtn.classList.add("square");
-  if (
-    isActivePlayerBoard &&
-    square &&
-    Object.prototype.hasOwnProperty.call(square, "isSunk")
-  ) {
-    squareBtn.classList.add("ship");
-  }
   if (isActivePlayerBoard) {
     squareBtn.classList.add("cursor-auto");
     squareBtn.setAttribute("disabled", "");
+    if (square && Object.prototype.hasOwnProperty.call(square, "isSunk")) {
+      squareBtn.classList.add("ship");
+    }
+  } else {
+    squareBtn.addEventListener("click", onSquareBtnClick);
+  }
+  if (square === "miss") {
+    squareBtn.classList.add("miss");
+  } else if (square === "hit") {
+    squareBtn.classList.add("hit");
   }
   squareBtn.dataset.rowIndex = rowIndex;
   squareBtn.dataset.columnIndex = columnIndex;
   return squareBtn;
 }
 
-export function renderBoard(el, board, isActivePlayerBoard) {
+export function renderBoard(el, board, isActivePlayerBoard, onSquareBtnClick) {
   const boardDiv = document.createElement("div");
   boardDiv.classList.add("board");
   board.forEach((row, i) => {
     row.forEach((square, j) => {
-      const squareBtn = createSquareBtn(square, isActivePlayerBoard, i, j);
+      const squareBtn = createSquareBtn(
+        square,
+        isActivePlayerBoard,
+        i,
+        j,
+        onSquareBtnClick,
+      );
       boardDiv.append(squareBtn);
     });
   });
   el.append(boardDiv);
+}
+
+export function clearBoards(boardContainers, onSquareBtnClick) {
+  boardContainers.forEach((boardContainer) => {
+    const squareBtns = boardContainer.querySelectorAll(".square");
+    squareBtns.forEach((squareBtn) => {
+      squareBtn.removeEventListener("click", onSquareBtnClick);
+    });
+    boardContainer.innerHTML = "";
+  });
+}
+
+export function disableBoard(boardContainer) {
+  const squareBtns = boardContainer.querySelectorAll(".square");
+  squareBtns.forEach((squareBtn) => {
+    squareBtn.classList.add("cursor-auto");
+    squareBtn.setAttribute("disabled", "");
+  });
 }
